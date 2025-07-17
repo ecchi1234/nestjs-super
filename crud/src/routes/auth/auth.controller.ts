8a69542d148cc79dab9a09f0cb4188d67d417720
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2025-07-17 17:47:32 +0700

    18: Adds global serialization and response DTO for Auth
    Introduces the `ClassSerializerInterceptor` globally to enhance serialization across the application.
    
    Updates the `AuthController` to use `SerializeOptions` for the `register` endpoint, ensuring consistent response structure.
    
    Defines a new `RegisterResDTO` with excluded sensitive fields (e.g., password) for secure and structured API responses.

--- crud/src/routes/auth/auth.controller.ts
+++ crud/src/routes/auth/auth.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, SerializeOptions } from '@nestjs/common'
 import { AuthService } from 'src/routes/auth/auth.service'
-import { RegisterBodyDTO } from 'src/routes/auth/auth.dto'
+import { RegisterBodyDTO, RegisterResDTO } from 'src/routes/auth/auth.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @SerializeOptions({ type: RegisterResDTO })
   @Post('register')
   register(@Body() body: RegisterBodyDTO) {
     return this.authService.register(body)
--- crud/src/routes/auth/auth.dto.ts
+++ crud/src/routes/auth/auth.dto.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer'
 import { IsString } from 'class-validator'
 
 export class LoginBodyDTO {
@@ -13,4 +14,18 @@ export class RegisterBodyDTO extends LoginBodyDTO {
 
   @IsString()
   confirmPassword: string
+}
+
+export class RegisterResDTO {
+  id: number
+  email: string
+  name: string
+  @Exclude()
+  password: string
+  createdAt: Date
+  updatedAt: Date
+
+  constructor(partial: Partial<RegisterResDTO>) {
+    Object.assign(this, partial)
+  }
 }
